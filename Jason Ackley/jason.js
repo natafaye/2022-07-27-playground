94904d69938b806181cb64ffc820c7da7a58119c
Jason Ackley <Sparkyack@gmail.com>
2022-09-09 16:59:15 -0500

    mission log 5

--- Jason Ackley/jason.js
+++ Jason Ackley/jason.js
@@ -1,4 +1,5 @@
 console.log("Hello friends I am slow but I will get it!");
 console.log("hello world");
 console.log("I think Im starting to get the hang of this...Finally!");
-
+console.log("This is my mission log. Things are going crazy!");
+console.log("ok finally done with the war game. That was a tough one! Starting on html. in the mean time, time to practice some javascript")
